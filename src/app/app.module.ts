81316b13093fe0831f0a60d9643dbbbcd9fcc2b3
aditya_kk <adityakrishnakumar12@gmail.com>
2021-08-27 14:25:41 +0530

    Added Slider through Angular Materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { RiskmComponent } from './riskm/riskm.component';
 import { HeaderComponent } from './header/header.component';
 import { PsrowComponent } from './riskm/psrow/psrow.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { PsrowComponent } from './riskm/psrow/psrow.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
